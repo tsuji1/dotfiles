f53607bb44ec0bde09b7854b073f3af12638569d
Yuzuki Tsuji <yuzu777yuzu0@gmail.com>
2025-08-15 15:21:21 +0900

    feature:  ssh copy and diag

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -14,14 +14,53 @@ vim.opt.tabstop = 2
 
 vim.opt.scrolloff = 3
 
-vim.opt.whichwrap = 'b,s,h,l,<,>,[,],~'
+vim.opt.whichwrap = "b,s,h,l,<,>,[,],~"
 
--- share clipboard with OS 
-vim.opt.clipboard:append('unnamedplus,unnamed')
+-- すでに set clipboard=unnamedplus しているなら、SSH のときだけ無効化
+if vim.env.SSH_TTY or vim.env.SSH_CONNECTION then
+	vim.opt.clipboard = "" -- "+y でなく普通の y で送る構成
+else
+	-- share clipboard with OS
+	vim.opt.clipboard:append("unnamedplus,unnamed")
+end
 
 -- InitLua
-require('user_command')
-require('config.lazy')
+require("user_command")
+require("config.lazy")
 
+-- 反応速度
+vim.o.updatetime = 250
 
+-- カーソルを止めたら、その場所の警告理由をポップアップ
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+	callback = function()
+		vim.diagnostic.open_float(nil, {
+			focus = false,
+			scope = "cursor",
+			source = "if_many",
+			header = "",
+			close_events = { "CursorMoved", "CursorMovedI", "InsertEnter", "FocusLost" },
+		})
+	end,
+})
 
+-- ← 以前入れていた「CursorMovedでvim.diagnostic.hide()」は削除！
+
+vim.keymap.set("n", "gl", function()
+	vim.diagnostic.open_float(nil, { focus = false, scope = "cursor" })
+end, { desc = "Show diagnostic under cursor" })
+
+vim.diagnostic.config({
+	virtual_text = false, -- 行内の文字は非表示
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "E",
+			[vim.diagnostic.severity.WARN] = "W",
+			[vim.diagnostic.severity.INFO] = "I",
+			[vim.diagnostic.severity.HINT] = "H",
+		},
+		numhl = false, -- 番号欄のハイライト（お好み）
+	},
+	underline = true,
+	severity_sort = true,
+})
