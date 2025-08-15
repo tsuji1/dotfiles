82bcdc24b85f401783409f0e79551bf6f215eb37
Yuzuki Tsuji <yuzu777yuzu0@gmail.com>
2025-08-15 15:20:16 +0900

    feature: vim-plugin finder init

--- .config/nvim/lua/plugins/finder.lua
+++ .config/nvim/lua/plugins/finder.lua
@@ -1,22 +1,20 @@
-
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    -- 例: 起動後しばらくして自動ロード（遅延は残す）
-    event = "VeryLazy",
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		-- 例: 起動後しばらくして自動ロード（遅延は残す）
+		event = "VeryLazy",
 
-    config = function()
-      local telescope = require("telescope")
-      telescope.setup({})
+		config = function()
+			local telescope = require("telescope")
+			telescope.setup({})
 
-      -- ここでなら OK（ロード済み）
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope: find files" })
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep,  { desc = "Telescope: live grep"  })
-      vim.keymap.set("n", "<leader>fb", builtin.buffers,    { desc = "Telescope: buffers"    })
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags,  { desc = "Telescope: help tags"  })
-    end,
-  },
+			-- ここでなら OK（ロード済み）
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope: find files" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope: live grep" })
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope: buffers" })
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope: help tags" })
+		end,
+	},
 }
-
