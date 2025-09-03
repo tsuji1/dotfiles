70792f024d29b47a108a3d97e05594f89650bd15
Yuzuki Tsuji <yuzu777yuzu0@gmail.com>
2025-09-03 23:25:55 +0900

    feature: add keymap

--- .config/nvim/lua/plugins/filer.lua
+++ .config/nvim/lua/plugins/filer.lua
@@ -1,32 +1,41 @@
 return {
-  {
-    "nvim-tree/nvim-tree.lua",
-    event = "VeryLazy",
+	{
+		"nvim-tree/nvim-tree.lua",
+		event = "VeryLazy",
 
-    config = function()
-      local nvim_tree = require("nvim-tree")
+		config = function()
+			local nvim_tree = require("nvim-tree")
 
-      nvim_tree.setup({
-        -- nvim-tree バッファに入った時だけ走る
-        on_attach = function(bufnr)
-          local api = require("nvim-tree.api")
+			nvim_tree.setup({
+				-- nvim-tree バッファに入った時だけ走る
+				on_attach = function(bufnr)
+					local api = require("nvim-tree.api")
 
-          -- nvim-tree が用意してるデフォルト mapping を先に適用
-          api.config.mappings.default_on_attach(bufnr)
+					-- nvim-tree が用意してるデフォルト mapping を先に適用
+					api.config.mappings.default_on_attach(bufnr)
 
-          -- ここから“追 加 / 上 書 き”はバッファローカルになる
-          local function map_opts(desc)
-            return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-          end
+					-- ここから“追 加 / 上 書 き”はバッファローカルになる
+					local function map_opts(desc)
+						return {
+							desc = "nvim-tree: " .. desc,
+							buffer = bufnr,
+							noremap = true,
+							silent = true,
+							nowait = true,
+						}
+					end
 
-          vim.keymap.set("n", "<C-t>", api.tree.change_root_to_parent, map_opts("Up"))
-          vim.keymap.set("n", "?",     api.tree.toggle_help,           map_opts("Help"))
-        end,
-      })
+					vim.keymap.set("n", "<C-t>", api.tree.change_root_to_parent, map_opts("Up"))
+					vim.keymap.set("n", "?", api.tree.toggle_help, map_opts("Help"))
+				end,
+			})
 
-      -- これはグローバルでOK（トグル起動用）
-      vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
-    end,
-  },
+			-- これはグローバルでOK（トグル起動用）
+			--
+			-- -- NvimTree からエディタへフォーカスを戻すショートカット
+			vim.keymap.set("n", "<leader>eo", "<cmd>wincmd p<CR>", { desc = "Focus back to editor" })
+			vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+			vim.keymap.set("n", "<leader>ef", "<cmd>NvimTreeFocus<CR>", { desc = "NvimTreeFocus" })
+		end,
+	},
 }
-
