aeb38393034f003beae8adba702ccf4c5a75feda
Yuzuki Tsuji <yuzu777yuzu0@gmail.com>
2025-09-03 23:27:21 +0900

    feature: add window edit

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -28,8 +28,37 @@ end
 require("user_command")
 require("config.lazy")
 
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true, desc = "Insertモードを抜ける" })
+
+-- 分割
+vim.keymap.set("n", "<leader>sh", "<cmd>split<CR>", { desc = "横分割" })
+vim.keymap.set("n", "<leader>sv", "<cmd>vsplit<CR>", { desc = "縦分割" })
+vim.keymap.set("n", "<leader>sc", "<cmd>close<CR>", { desc = "ウィンドウ閉じる" })
+vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "サイズ均等" })
+
+-- 移動
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+-- リサイズ（矢印キー派）
+vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<CR>")
+vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<CR>")
+vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -4<CR>")
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +4<CR>")
+
+-- active/inactive window の背景を差別化
+vim.api.nvim_set_hl(0, "Normal", { bg = "#1e1e2e" }) -- 非アクティブ
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "#161622" }) -- アクティブ以外
+
+vim.opt.laststatus = 2 -- 各分割にステータスライン
+
 -- 反応速度
 vim.o.updatetime = 250
+
+vim.keymap.set("n", "<A-h>", "gT", { desc = "前のタブ" })
+vim.keymap.set("n", "<A-l>", "gt", { desc = "次のタブ" })
 
 -- カーソルを止めたら、その場所の警告理由をポップアップ
 vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
