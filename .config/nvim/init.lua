99bf48ce0d5eaaf98bcf4d879a468c3537949d77
Yuzuki Tsuji <yuzu777yuzu0@gmail.com>
2025-09-19 15:37:25 +0900

    delete:unused writing

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -17,12 +17,12 @@ vim.opt.scrolloff = 3
 vim.opt.whichwrap = "b,s,h,l,<,>,[,],~"
 
 -- すでに set clipboard=unnamedplus しているなら、SSH のときだけ無効化
-if vim.env.SSH_TTY or vim.env.SSH_CONNECTION then
-	vim.opt.clipboard = "" -- "+y でなく普通の y で送る構成
-else
-	-- share clipboard with OS
-	vim.opt.clipboard:append("unnamedplus,unnamed")
-end
+-- if vim.env.SSH_TTY or vim.env.SSH_CONNECTION then
+-- 	vim.opt.clipboard = "" -- "+y でなく普通の y で送る構成
+-- else
+-- 	-- share clipboard with OS
+-- 	vim.opt.clipboard:append("unnamedplus,unnamed")
+-- end
 
 -- InitLua
 require("user_command")
@@ -47,18 +47,6 @@ vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<CR>")
 vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<CR>")
 vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -4<CR>")
 vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +4<CR>")
-
-vim.keymap.set("n", "<F5>", function()
-	local file = vim.fn.expand("%")
-	local ft = vim.bo.filetype
-	if ft == "python" then
-		vim.cmd("!python3 " .. file)
-	elseif ft == "c" then
-		vim.cmd("!gcc " .. file .. " -o " .. vim.fn.expand("%:r") .. " && " .. vim.fn.expand("%:r"))
-	elseif ft == "cpp" then
-		vim.cmd("!g++ " .. file .. " -o " .. vim.fn.expand("%:r") .. " && " .. vim.fn.expand("%:r"))
-	end
-end)
 
 -- active/inactive window の背景を差別化
 -- vim.api.nvim_set_hl(0, "Normal", { bg = "#1e1e2e" }) -- 非アクティブ
@@ -87,21 +75,21 @@ vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
 
 -- ← 以前入れていた「CursorMovedでvim.diagnostic.hide()」は削除！
 
-vim.keymap.set("n", "gl", function()
-	vim.diagnostic.open_float(nil, { focus = false, scope = "cursor" })
-end, { desc = "Show diagnostic under cursor" })
-
-vim.diagnostic.config({
-	virtual_text = false, -- 行内の文字は非表示
-	signs = {
-		text = {
-			[vim.diagnostic.severity.ERROR] = "E",
-			[vim.diagnostic.severity.WARN] = "W",
-			[vim.diagnostic.severity.INFO] = "I",
-			[vim.diagnostic.severity.HINT] = "H",
-		},
-		numhl = false, -- 番号欄のハイライト（お好み）
-	},
-	underline = true,
-	severity_sort = true,
-})
+-- vim.keymap.set("n", "gl", function()
+-- 	vim.diagnostic.open_float(nil, { focus = false, scope = "cursor" })
+-- end, { desc = "Show diagnostic under cursor" })
+--
+-- vim.diagnostic.config({
+-- 	virtual_text = false, -- 行内の文字は非表示
+-- 	signs = {
+-- 		text = {
+-- 			[vim.diagnostic.severity.ERROR] = "E",
+-- 			[vim.diagnostic.severity.WARN] = "W",
+-- 			[vim.diagnostic.severity.INFO] = "I",
+-- 			[vim.diagnostic.severity.HINT] = "H",
+-- 		},
+-- 		numhl = false, -- 番号欄のハイライト（お好み）
+-- 	},
+-- 	underline = true,
+-- 	severity_sort = true,
+-- })
