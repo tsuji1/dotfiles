dd044b05cc4ddd7110968b8a299c91e1e2b9003e
Yuzuki Tsuji <yuzu777yuzu0@gmail.com>
2025-11-19 23:29:34 +0900

    feature: add aerial key setting

--- .config/nvim/lua/plugins/codeoutline.lua
+++ .config/nvim/lua/plugins/codeoutline.lua
@@ -11,11 +11,12 @@ return -- lazy.nvim を使ったインストール例
 			-- ここで細かい設定ができますが、デフォルトでも十分使えます
 			on_attach = function(bufnr)
 				-- ジャンプ用のキーマップなどを設定
-				vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr, desc = "Previous symbol" })
-				vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr, desc = "Next symbol" })
+    vim.keymap.set("n", "<leader>aj", require("aerial").next)
+    vim.keymap.set("n", "<leader>ak", require("aerial").prev)
 			end,
 		})
 		-- F4キーでアウトラインの表示/非表示を切り替える
+    vim.keymap.set("n", "<leader>ao", "<cmd>AerialToggle!<cr>")
 		vim.keymap.set("n", "<F4>", "<cmd>AerialToggle!<CR>", { desc = "Toggle Aerial outline" })
 	end,
 }
